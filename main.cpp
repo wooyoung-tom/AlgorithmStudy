1d79a8b3f30e7737ae34d59b3778f3935a70f3ae
WooYoung Choi <djdj7087@kakao.com>
2020-11-15 15:52:47 +0900

    1115 K번째 수 1차시도 시간초과

--- main.cpp
+++ main.cpp
@@ -10,12 +10,29 @@
 
 using namespace std;
 
-void input() {
+// N(1 ≤ N ≤ 5,000,000)과 K (1 ≤ K ≤ N)
+int N, K;
+// 숫자 담을 vector
+vector<int> container;
 
+bool compare(const int &first, const int &second) {
+    return first < second;
+}
+
+void input() {
+    cin >> N >> K;
+
+    for (int i = 0; i < N; ++i) {
+        int temp;
+        cin >> temp;
+        container.push_back(temp);
+    }
 }
 
 void solution() {
+    sort(container.begin(), container.end(), compare);
 
+    cout << container[K - 1];
 }
 
 int main() {
