d382b2cb5d5a77e60f48ff21acc888c8baa856a9
WooYoung Choi <djdj7087@kakao.com>
2020-12-31 09:56:28 +0900

    1231 하노이 완료

--- main.cpp
+++ main.cpp
@@ -9,33 +9,10 @@
 
 using namespace std;
 
-// 첫 번째 장대에 쌓인 원판의 개수 N (1 ≤ N ≤ 20)
-int N;
-// 옮긴 횟수
-int K;
-
-void hanoi(int num, int start, int end, int mid) {
-    if (num == 1) {
-        printf("%d -> %d\n", start, end);
-        return;
-    }
-
-    hanoi(num - 1, start, mid, end);
-    printf("%d -> %d\n", start, end);
-    hanoi(num - 1, mid, end, start);
-}
-
 void input() {
-    scanf("%d", &N);
-
-    hanoi(N, 1, 3, 2);
-}
-
-void solution() {
 
 }
 
 int main() {
     input();
-    solution();
 }
--- week6_recursion/retry/11729_하노이 탑 이동 순서.cpp
+++ week6_recursion/retry/11729_하노이 탑 이동 순서.cpp
@@ -2,3 +2,44 @@
 // Created by 최우영 on 2020/12/30.
 //
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 첫 번째 장대에 쌓인 원판의 개수 N (1 ≤ N ≤ 20)
+int N;
+// 옮긴 횟수
+int K;
+// 옮긴 방향 담을 vector
+vector<pair<int, int>> doVector;
+
+void hanoi(int num, int start, int end, int mid) {
+    if (num == 1) {
+        K++;
+        doVector.push_back(make_pair(start, end));
+        return;
+    }
+
+    hanoi(num - 1, start, mid, end);
+    K++;
+    doVector.push_back(make_pair(start, end));
+    hanoi(num - 1, mid, end, start);
+}
+
+void input() {
+    scanf("%d", &N);
+
+    hanoi(N, 1, 3, 2);
+
+    printf("%d\n", K);
+
+    for (int i = 0; i < doVector.size(); ++i) {
+        printf("%d %d\n", doVector[i].first, doVector[i].second);
+    }
+}
+
+int main() {
+    input();
+    solution();
+}
