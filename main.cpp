f02818b44c58b962660c1e54071f36febb40f566
WooYoung Choi <djdj7087@kakao.com>
2021-01-05 00:20:31 +0900

    0104 연산자 끼워넣기 진행중

--- main.cpp
+++ main.cpp
@@ -5,85 +5,32 @@
 #include <iostream>
 #include <vector>
 
-#define MAX 9
+#define MAX 11 + 1
 
 using namespace std;
 
-// 좌표 구조체
-struct Point {
-    int row, col;
-};
-
-// 스도쿠 판
-int sudoku[MAX][MAX];
-bool isVisited[MAX][MAX];
-// 채워 넣어야 할 좌표 담을 vector
-vector<Point> pVector;
-
-void printSudoku() {
-    for (int i = 0; i < MAX; ++i) {
-        for (int j = 0; j < MAX; ++j) {
-            printf("%d ", sudoku[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-bool isPromising(int targetNum, Point point) {
-    for (int i = 0; i < MAX; ++i) {
-        // 1. 같은 row 에 있으면 유망하지 않음
-        if (sudoku[i][point.col] == targetNum) return false;
-        // 2. 같은 col 에 있으면 유망하지 않음
-        if (sudoku[point.row][i] == targetNum) return false;
-    }
-
-    // 3. 현재 좌표의 범위 내에 있는 3x3 배열에도 존재하면 안된다.
-    int downSizeRow = (point.row / 3) * 3;
-    int downSizeCol = (point.col / 3) * 3;
-    for (int row = downSizeRow; row < downSizeRow + 3; ++row) {
-        for (int col = downSizeCol; col < downSizeCol + 3; ++col) {
-            // 내꺼 빼고 비교해야 함.
-            if (row == point.row && col == point.col) continue;
-            if (sudoku[row][col] == targetNum) return false;
-        }
-    }
-
-    return true;
-}
+// 수의 개수 N(2 ≤ N ≤ 11)
+int N;
+// A1, A2, ..., AN이 주어진다. (1 ≤ Ai ≤ 100)
+int A[MAX];
+// 합이 N-1인 4개의 정수가 주어지는데,
+// 차례대로 덧셈(+)의 개수, 뺄셈(-)의 개수, 곱셈(×)의 개수, 나눗셈(÷)의 개수
+int operation[4];
 
 void input() {
-    for (int row = 0; row < MAX; ++row) {
-        for (int col = 0; col < MAX; ++col) {
-            scanf("%d", &sudoku[row][col]);
+    scanf("%d", &N);
 
-            if (sudoku[row][col] == 0) {
-                pVector.push_back(Point{row, col});
-            }
-        }
-    }
-}
-
-void solveSudoKu(int depth, Point point) {
-    if (depth == pVector.size()) {
-        printSudoku();
-
-        // 프로그램을 끝내야 한다.
-        exit(0);
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &A[i]);
     }
 
-    for (int i = 1; i <= MAX; ++i) {
-        if (!isVisited[point.row][point.col] && isPromising(i, point)) {
-            isVisited[point.row][point.col] = true;
-            sudoku[point.row][point.col] = i;
-            solveSudoKu(depth + 1, pVector[depth + 1]);
-            sudoku[point.row][point.col] = 0;
-            isVisited[point.row][point.col] = false;
-        }
+    for (int i = 0; i < 4; ++i) {
+        scanf("%d", &operation[i]);
     }
 }
 
 int main() {
     input();
 
-    solveSudoKu(0, pVector[0]);
+    
 }
