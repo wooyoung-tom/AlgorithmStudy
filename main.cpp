ed1808caf41bf506f6a670654d646f925d9ea19f
WooYoung Choi <djdj7087@kakao.com>
2021-01-19 22:37:06 +0900

    0119 등굣길 시간초과 해결중

--- main.cpp
+++ main.cpp
@@ -6,26 +6,17 @@
 
 using namespace std;
 
-struct Point {
-    int col, row;
-};
-
 int way[101][101];
 
-Point pMove[2] = {Point{1, 0}, Point{0, 1}};
-
-bool isPuddle(int col, int row, vector<vector<int>> puddles) {
+bool isPossible(int col, int row, int m, int n, vector<vector<int>> puddles) {
     for (int i = 0; i < puddles.size(); ++i) {
         int pCol = puddles[i][0];
         int pRow = puddles[i][1];
 
-        if (col == pCol && row == pRow) return true;
+        if (col == pCol && row == pRow) return false;
     }
-    return false;
-}
 
-bool isInRange(int mCol, int mRow, int m, int n) {
-    return mCol <= m && mRow <= n;
+    return col <= m && row <= n;
 }
 
 int solution(int m, int n, vector<vector<int>> puddles) {
@@ -37,19 +28,13 @@ int solution(int m, int n, vector<vector<int>> puddles) {
     // (1, 1) 부터 순회 시작
     for (int i = 1; i <= n; ++i) {
         for (int j = 1; j <= m; ++j) {
-            // 현재 있는 곳이 잠겨있으면 continue
-            if (isPuddle(j, i, puddles)) continue;
-
-            // 오른쪽 및 아래로 이동
-            for (int k = 0; k < 2; ++k) {
-                int mCol = j + pMove[k].col;
-                int mRow = i + pMove[k].row;
-
-                // 웅덩이인지, 범위 내에 있는지 확인
-                if (isPuddle(mCol, mRow, puddles)) continue;
-                if (!isInRange(mCol, mRow, m, n)) continue;
-
-                way[mCol][mRow] += way[j][i];
+            // 1. 오른쪽
+            if (isPossible(j + 1, i, m, n, puddles)) {
+                way[j + 1][i] = (way[j + 1][i] + way[j][i]) % TARGET;
+            }
+            // 2. 아래
+            if (isPossible(j, i + 1, m, n, puddles)) {
+                way[j][i + 1] = (way[j][i] + way[j][i + 1]) % TARGET;
             }
         }
     }
@@ -59,13 +44,4 @@ int solution(int m, int n, vector<vector<int>> puddles) {
     printf("%d", answer);
 
     return answer;
-}
-
-int main() {
-    int m = 4, n = 3;
-    vector<vector<int>> puddles = {
-            {2, 2}
-    };
-
-    solution(m, n, puddles);
 }
