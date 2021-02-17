a940ebbca6576a7c88c7f9172c997c6e4521c725
WooYoung Choi <djdj7087@kakao.com>
2021-02-17 22:23:11 +0900

    0217 징검다리 풀이중

--- main.cpp
+++ main.cpp
@@ -4,33 +4,21 @@
 
 using namespace std;
 
-typedef long long ll;
+int solution(int distance, vector<int> rocks, int n) {
+    int answer = 0;
 
-long long solution(int n, vector<int> times) {
-    long long answer = 0;
+    int left = 0, right = distance;
 
-    // left 1로 초기화, right 가장 큰 수 * n 으로 초기화
-    ll left = 1;
-    ll right = (ll) *max_element(times.begin(), times.end()) * n;
+    // 바위 위치를 오름차순으로 정렬
+    sort(rocks.begin(), rocks.end());
 
+    // 이분탐색을 진행
     while (left <= right) {
-        ll mid = (left + right) / 2;
-        ll res = 0;
+        int mid = (left + right) / 2;
 
-        // mid 에서 해당 시간을 나누면 그 시간에 가능한 사람 수가 된다.
-        // 그렇게 모든 시간을 나누어주고 모두 더한 값을 구한다.
-        for (int i = 0; i < times.size(); ++i) {
-            res += (ll) mid / times[i];
-        }
+        for (int i = 0; i < rocks.size(); ++i) {
 
-        // n 보다 작으면 n 명을 처리하지 못했다는 뜻이 된다.
-        // n 보다 크면 n 명을 처리하였다는 뜻이 된다.
-        if (res < n) left = mid + 1;
-        else {
-            right = mid - 1;
-            answer = mid;
         }
     }
-
     return answer;
 }
