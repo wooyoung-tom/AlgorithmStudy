d9c324a3215bc73e0c414bf8a716b509d0d33203
WooYoung Choi <djdj7087@gmail.com>
2021-03-06 16:50:07 +0900

    0306 단어 퍼즐 제발;;

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <climits>
+#include <set>
+
 using namespace std;
 
-int solution(vector<string> strs, string t)
-{
+int solution(vector<string> strs, string t) {
     int answer = 0;
 
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    cout << "Hello Cpp" << endl;
+    int dp[t.length()];
+
+    // string t 의 뒤에서 부터 하나씩 늘려가면서 진행한다.
+    for (int i = t.length() - 1; i >= 0; i--) {
+        // 하나씩 늘려준 string 저장할 변수
+        string tmp = "";
+
+    }
 
     return answer;
+}
+
+int main() {
+    vector<string> strs = {"ba","na","n","a"};
+    string t = "banana";
+
+    solution(strs, t);
 }
